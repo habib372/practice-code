ff06d9719f1c1a8ddf8b8bdd63104115fbe52943
habib372 <habibsoft372@gmail.com>
2022-12-14 15:17:13 +0600

    updated

--- dynamic_page/dynamic_page.php
+++ dynamic_page/dynamic_page.php
@@ -9,13 +9,7 @@ $color = Color::where('status',0)->first();
 return view('frontend.page',compact('page','color'));
 }
 
-<<<<<<< HEAD
-{{ (request()->is('page*')) ? 'active' : '' }}
 
-{{ (request()->is('about*')) ? 'active' : '' }}
-
-=======
->>>>>>> 856fc22d979c458a84b741b617e4c34da407f292
 <!-- navbar link page: -->
 @php
 $page = App\Page::where('parent_id', 14)->get();
@@ -24,20 +18,15 @@ $page = App\Page::where('parent_id', 14)->get();
     <label for="btn-2-services" class="show">Services +</label>
     <a href="#">Services</a>
 
-<<<<<<< HEAD
     <input type="checkbox" id="btn-2-services">
     <ul style="background-color: {{$color->color_code}};">
         @foreach($page as $sub_data)
-        <li style="background-color: {{$color->color_code}};"><a href="{{url('services/')}}/{{strtolower(str_replace(" ", "-",$sub_data->page_name))}}">{{$sub_data->page_name}}</a></li>
+        <li ><a href="{{url('services/')}}/{{strtolower(str_replace(" ", "-",$sub_data->page_name))}}">{{$sub_data->page_name}}</a></li>
         @endforeach
     </ul>
 </li>
-=======
-                                <input type="checkbox" id="btn-2-services">
-                                <ul style="background-color: {{$color->color_code}};">
-                                    @foreach($page as $sub_data)
-                                    <li style="background-color: {{$color->color_code}};"><a href="{{url('services/')}}/{{strtolower(str_replace(" ", "-",$sub_data->page_name))}}">{{$sub_data->page_name}}</a></li>
-                                    @endforeach
-                                </ul>
-                            </li>
->>>>>>> 856fc22d979c458a84b741b617e4c34da407f292
+
+<!-- navbar active inactive -->
+{{ (request()->is('page*')) ? 'active' : '' }}
+
+{{ (request()->is('about*')) ? 'active' : '' }}
---
+++ query and migrations/delete_with_image.php
@@ -0,0 +1,14 @@
+
+
+ public function delete($id)
+    {
+        $data = Page::find($id);
+        if (file_exists( public_path().'/uploads/page/'.$data->image)) {
+            $image_path = public_path().'uploads/page/'.$data->image;
+            unlink("uploads/page/".$data->image);
+        }
+        $data->delete();
+
+        Toastr::success('Category Delete successfully :)','Success');
+        return back();
+    }
