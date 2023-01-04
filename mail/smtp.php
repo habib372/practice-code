6337ccd6ee4dfcc4485fb360893b2c205790b674
habib372 <habibsoft372@gmail.com>
2023-01-04 14:45:26 +0600

    updated

--- mail/smtp.php
+++ mail/smtp.php
@@ -1,7 +1,7 @@
 MAIL_MAILER=sendmail
 MAIL_HOST=smtp.gmail.com
 MAIL_PORT=587
-MAIL_USERNAME=info@texroute.com
+MAIL_USERNAME=webmail
 MAIL_PASSWORD=''
 MAIL_ENCRYPTION=tls
 MAIL_FROM_ADDRESS=
@@ -12,8 +12,25 @@ MAIL_FROM_NAME="${Buying House}"
 MAIL_MAILER=sendmail
 MAIL_HOST=mail.bdrentacar.com
 MAIL_PORT=465
-MAIL_USERNAME=info@bdrentacar.com
-MAIL_PASSWORD=2z}9^]l&ec+L
+MAIL_USERNAME=webmail
+MAIL_PASSWORD='password'
 MAIL_ENCRYPTION=ssl
 MAIL_FROM_ADDRESS=
 MAIL_FROM_NAME="BD RENT A CAR"
+
+
+MAIL_DRIVER=sendmail
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=465
+MAIL_USERNAME=gmail_address
+MAIL_PASSWORD='password'
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=
+MAIL_FROM_NAME="BD RENT A CAR"
+
+
+On the device or in the app, enter smtp.gmail.com as the server address.
+In the Port field, enter one of the following numbers:
+
+For SSL, enter 465.
+For TLS, enter 587.
