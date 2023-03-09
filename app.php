96179a26bb9fd5f546aa8ee935eea0f6042b248a
habib372 <habibsoft372@gmail.com>
2023-03-09 12:28:21 +0600

    updated

---
+++ app.php
@@ -0,0 +1,12 @@
+<section id="category-logo" class="py-5 mt-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="title-banner">
+                    <!--<img src="{{asset('uploads/allproducts.jpg')}}" no-repeat alt=""/>-->
+                    <p class="ml-3">Our All Products</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
