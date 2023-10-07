06b65e20db82fbfc7fdd22979ddf62899b71f8ef
habib372 <habibsoft372@gmail.com>
2023-10-07 11:13:04 +0600

    updated

--- query and migrations/created_formet.php
+++ query and migrations/created_formet.php
@@ -15,3 +15,6 @@ $temp['date'] = \Carbon\Carbon::parse($item->visit_date)->format('j M, Y'); //21
 
 <strong>Last updated on</strong> : {{ date("h:i A \o\\n d F Y", strtotime($user->updated_at)) }}
 <!-- Last Updated On: 06:20 PM on 11 September 2023  -->
+
+<p>Date : {{date("jS M, Y", strtotime($lastVisit->visit_date))}}</p>
+<!-- Date : 4th Oct, 2023 -->
