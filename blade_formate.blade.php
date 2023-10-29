70509fae71a18caaade2cb395273ebdafa28baf3
habib372 <habibsoft372@gmail.com>
2023-10-29 11:18:55 +0600

    updated

---
+++ blade_formate.blade.php
@@ -0,0 +1,16 @@
+@extends('frontend.layouts.app')
+
+@section('page_title', 'Fighting Cancer Bangladesh - FCB | All Medical History' )
+
+@section('styles')
+@endsection
+
+@section('content')
+Medical History
+
+{{-- @section('scripts')
+    <script>
+    </script>
+@stop --}}
+
+@endsection
