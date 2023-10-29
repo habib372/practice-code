76a97d35c5d7eba011a41d7709f21e82969d5fa8
habib372 <habibsoft372@gmail.com>
2023-10-29 18:16:42 +0600

    updated

--- app3.php
+++ app3.php
@@ -1,1 +1,32 @@
-ddgfs
+<div class="form-group m-form__group m-attach">
+    <div class="row">
+        <div class="col-md-11" id="attachmentsHolder">
+            <!-- Ajax Content will load here -->
+            <label for="visit_reason">Medical Attachments <small>(Max file size: 10MB; Allowed type: pdf or scan image)</small> <span class="text-danger">*</span></label>
+            <div class="row mb-2 attach_title">
+                <div class="col-md-5">Attachment Title</div>
+                <div class="col-md-4">Attachments file</div>
+            </div>
+            @for ($i = 0; $i < 1; $i++)
+            <div class="row mb-2" id="attachmentsHolder{{$i}}">
+                <div class="col-md-5">
+                    <input type="text" class="form-control m-input" name="attachs[{{$i}}][title]" id="title_{{$i}}">
+                </div>
+                <div class="col-md-4">
+                    <input type="file" class="form-control m-input file_upload" name="attachs[{{$i}}][file]" id="files_{{$i}}">
+                </div>
+                @if ($i == 0)
+                <div class="col-md-3">
+                    <a href="javascript:void(0)" id="addMoreAttach" class="m_btn-custom">
+                        <span>
+                            <i class="fa fa-plus"></i>
+                            <span>Add More</span>
+                        </span>
+                    </a>
+                </div>
+                @endif
+        </div>
+        @endfor
+    </div>
+</div>
+</div>
--- jquery/1. add_more_data_ajax.blade.php
+++ jquery/1. add_more_data_ajax.blade.php
@@ -5,7 +5,7 @@
          <div class="col-md-5"><strong>Attachment Title</strong></div>
          <div class="col-md-4"><strong>Attachments file</strong></div>
      </div>
-     @for($i=1; $i<2; $i++)
+     @for($i=0; $i<1; $i++)
      <div class="row  mb-2" id="attachmentsHolder">
          <div class="col-md-5">
              <input type="text" class="form-control m-input" name="attachs[{{$i}}][title]" id="title_{{$i}}">
@@ -13,7 +13,7 @@
          <div class="col-md-4">
              <input type="file" class="form-control m-input file_upload" name="attachs[{{$i}}][file]" id="files_{{$i}}">
          </div>
-         @if($i==1)
+         @if($i==0)
          <div class="col-md-3">
              <a href="javascript:void(0)" id="addMoreAttach" class="btn m_btn-custom">
                  <span>
@@ -28,59 +28,31 @@
  </div>
 
 
- <script>
-        var attrIndex = 2;
-        $('body').on('click', '#addMoreAttributes', function () {
+<script>
 
-            $('#addMoreAttributes').remove();
-
-            var html = '<div id="attribute-'+attrIndex+'" class="row mb-2">'+
-                    '<div class="col-md-5">'+
-                        '<input type="text" class="form-control m-input" name="visit_attributes_name['+attrIndex+']" id="visit_attributes_name_'+attrIndex+'">'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                        '<input type="text" class="form-control m-input file_upload" name="visit_attributes_value['+attrIndex+']" id="visit_attributes_value_'+attrIndex+'">'+
-                    '</div>'+
-
-                    '<div class="col-md-3">'+
-                        '<a id="addMoreAttributes" href="javascript:void(0)" class="btn m_btn-custom">'+
-                            '<span>'+
-                                '<i class="fa fa-plus"></i>'+
-                                '<span>Add More</span>'+
-                            '</span>'+
-                        '</a>'+
-                    '</div>'+
-                '</div>	';
-            attrIndex++;
-
-            $('#attributesHolder').append(html);
-        });
-
-
-        var attachIndex = 2;
+    var attachIndex = 1;
         $('body').on('click', '#addMoreAttach', function () {
-
             $('#addMoreAttach').remove();
 
-            var html = '<div id="attribute-'+attachIndex+'" class="row mb-2">'+
-                    '<div class="col-md-5">'+
-                        '<input type="text" class="form-control m-input" name="attachs['+attachIndex+'][title]" id="title_'+attrIndex+'">'+
-                    '</div>'+
-                    '<div class="col-md-4">'+
-                        '<input type="file" class="form-control m-input file_upload" name="attachs['+attachIndex+'][file]" id="files_'+attrIndex+'">'+
-                    '</div>'+
+            var html = '<div id="attribute-' + attachIndex + '" class="row mb-2">' +
+                '<div class="col-md-5">' +
+                '<input type="text" class="form-control m-input" name="attachs[' + attachIndex + '][title]" id="title_' + attachIndex + '">' +
+                '</div>' +
+                '<div class="col-md-4">' +
+                '<input type="file" class="form-control m-input file_upload" name="attachs[' + attachIndex + '][file]" id="files_' + attachIndex + '">' +
+                '</div>' +
 
-                    '<div class="col-md-3">'+
-                        '<a id="addMoreAttach" href="javascript:void(0)" class="btn m_btn-custom">'+
-                            '<span>'+
-                                '<i class="fa fa-plus"></i>'+
-                                '<span>Add More</span>'+
-                            '</span>'+
-                        '</a>'+
-                    '</div>'+
-                '</div>	';
+                '<div class="col-md-3">' +
+                '<a id="addMoreAttach" href="javascript:void(0)" class="btn m_btn-custom">' +
+                '<span>' +
+                '<i class="fa fa-plus"></i>' +
+                '<span>Add More</span>' +
+                '</span>' +
+                '</a>' +
+                '</div>' +
+                '</div>';
             attachIndex++;
 
             $('#attachmentsHolder').append(html);
         });
-    </script>
+</script>
