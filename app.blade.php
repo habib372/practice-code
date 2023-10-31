05e9f22c0ca726712f6e13b372194f9591460916
habib372 <habibsoft372@gmail.com>
2023-10-31 19:18:14 +0600

    updated

--- app.blade.php
+++ app.blade.php
@@ -1,43 +1,43 @@
 <section class="sponsorship-apply-form">
     <div class="container">
         <!-- webflow -->
-        <div class="row">
-            <div class="col-md-11 webflow">
-                <div class="mb-4 signup-steps-panel" id="myTab" role="tablist">
-                    <div class="signup-step-logo is-active">
-                        <div class="mb-2">Apply</div>
-                        <a data-toggle="tab" href="#step-1" role="tab">
-                            <div class="step-circle">
-                                <div class="index-text">1</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="signup-step-logo">
-                        <div class="mb-2">Review</div>
-                        <a data-toggle="tab" href="#step-2" role="tab">
-                            <div class="step-circle">
-                                <div class="index-text">2</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="signup-step-logo">
-                        <div class="mb-2">Recommend</div>
-                        <a data-toggle="tab" href="#step-3" role="tab">
-                            <div class="step-circle">
-                                <div class="index-text">3</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="signup-step-logo">
-                        <div class="mb-2">Approve</div>
-                         <a data-toggle="tab" href="#step-4" role="tab">
-                            <div class="step-circle">
-                                <div class="index-text">4</div>
-                            </div>
-                        </a>
+       <div class="row custom-margin">
+                <div class="col-md-12 webflow">
+                    <div class="mb-4 signup-steps-panel" id="myTab" role="tablist">
+                        <div class="signup-step-logo is-active" id="step_1">
+                            <div class="mb-2">Apply</div>
+                            <a data-toggle="tab" href="#step-1" role="tab">
+                                <div class="step-circle">
+                                    <div class="index-text">1</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="signup-step-logo " id="step_2">
+                            <div class="mb-2">Review</div>
+                            <a data-toggle="tab" href="#step-2" role="tab">
+                                <div class="step-circle">
+                                    <div class="index-text">2</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="signup-step-logo" id="step_3">
+                            <div class="mb-2">Recommend</div>
+                            <a data-toggle="tab" href="#step-3" role="tab">
+                                <div class="step-circle">
+                                    <div class="index-text">3</div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="signup-step-logo" id="step_4">
+                            <div class="mb-2">Approve</div>
+                            <a data-toggle="tab" href="#step-4" role="tab">
+                                <div class="step-circle">
+                                    <div class="index-text">4</div>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
         <!--/End webflow -->
 
@@ -102,43 +102,46 @@
         </div>
         <!--/End sponsorship form -->
 
-        <!--Step-2 form -->
-        <div class="row tab-pane " id="step-2">
-            <div class="col-md-11 sponsorship_form">
-                <h5 class="sponsorship_title">We have received your application and reviewing our Specialist Doctor </h5>
-                <div class="sponsorship_form_body">
-                    <p>Note:
-                        1. Please allow at least 3 weeks to review and process your application.
-                        2. Your application will be reviewed by very experienced oncologists and clinicians based in Bangladesh and Australia. Applications will be considered for funding only upon their positive recommendation.
-                        3. We have limited funds available. Hence, although the application is genuine and urgent, we may be able to fund all applications.
-                        4. We do not entertain any requests over phone, email, or </p>
+        <!-- Step-2 form -->
+            <div class="row custom-margin" id="step-2">
+                <div class="col-md-12 sponsorship_note">
+                    <h5 class="sponsorship_note_title">We have received your application and reviewing our Specialist Doctor</h5>
+                    <div class="sponsorship_note_body">
+                        <div class="sponsorship_content">
+                            <p>Our specialist review and investigate your information</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-         <!--/End Step-2 form -->
+            <!--/End Step-2 form -->
 
-         <!--Step-3 form -->
-        <div class="row tab-pane " id="step-3">
-            <div class="col-md-11 sponsorship_form">
-                <h5 class="sponsorship_title">Your application is being reviewed by our board members</h5>
-                <div class="sponsorship_form_body">
-                    <p>Our board memeber review and investigate your information. </p>
+            <!--Step-3 form -->
+            <div class="row custom-margin" id="step-3">
+                <div class="col-md-12 sponsorship_note">
+                    <h5 class="sponsorship_note_title">Your application is being reviewed by our board members</h5>
+                    <div class="sponsorship_note_body">
+                        <div class="sponsorship_content">
+                            <p>Our board memeber review and investigate your information. </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-         <!--/End Step-3 form -->
+            <!--/End Step-3 form -->
 
-          <!--Step-4 form -->
-        <div class="row tab-pane " id="step-4">
-            <div class="col-md-11 sponsorship_form">
-                <h5 class="sponsorship_title">Congratulation! You have own a sponsorship</h5>
-                <div class="sponsorship_form_body">
-                    <p>Note:
-                      We are approve your applications </p>
+            <!--Step-4 form -->
+            <div class="row custom-margin" id="step-4">
+                <div class="col-md-12 sponsorship_note">
+                    <h5 class="sponsorship_note_title">Congratulation! You have own a sponsorship</h5>
+                    <div class="sponsorship_note_body">
+                        <div class="sponsorship_content">
+                            <p> We are approve your applications </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-         <!--/End Step-4 form -->
+            <!--/End Step-4 form -->
+
+I want to add jquery when #step_1 hasClass 'is-active', then (#step-2, #step-3, #step-4) addCSS 'display:none', or when #step_2 hasClass 'is-active', then (#step-1, #step-3, #step-4) addCSS 'display:none' and (#step_1) addClass 'done', or when #step_3 hasClass 'is-active', then (#step-1, #step-2, #step-4) addCSS 'display:none' and (#step-1, #step-2) addClass 'done', or when #step_4 hasClass 'is-active', then (#step-1, #step-2, #step-3) addCSS 'display:none' and (#step-1, #step-2, #step_3) addClass 'done'
 
     </div>
 </section>
