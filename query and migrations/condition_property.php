cf2cae96634fbf34c5d0a9280bc31186c586d2b0
habib372 <habibsoft372@gmail.com>
2023-11-05 00:21:54 +0600

    updated

--- query and migrations/condition_property.php
+++ query and migrations/condition_property.php
@@ -1,23 +1,37 @@
 public function index()
     {
+
+
         $this->authorize('accessDashboard',Permission::class);
          $today = Carbon::today();
          $sixMonthsAgo = Carbon::now()->subMonths(6);
+         
          $todayspay = PropertyPayment::where('payment_status', 1)
             ->whereDate('created_at', $today)
             ->sum('price');
-        $thisMonthPay = PropertyPayment::where('created_at', '>=', Carbon::now()->subDays(30)->toDateTimeString())->sum('price');
+        $thisMonthPay = PropertyPayment::whereYear('created_at', Carbon::now()->year)
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->sum('price');
+        $lastMonthPay = PropertyPayment::where('created_at', '>=', Carbon::now()->subDays(30)->toDateTimeString())->sum('price');
+        $sixMonthPay = PropertyPayment::where('created_at', '>=', $sixMonthsAgo)->sum('price');
         $data = array
         (
-            'users' => DB::table('users')->count(),
             'categories' => DB::table('categories')->where('level', 1)->count(),
-            'totalpayment' => $todayspay,
             'subcategories' => DB::table('categories')->where('level', 2)->count(),
             'subchildcategories' => DB::table('categories')->where('level', 3)->count(),
             'brands' => DB::table('brands')->count(),
-            'products' => DB::table('properties')->count(),
+             // all users
+             'users' => DB::table('users')->count(),
+            //total ads
+            'allPropertyAds' => DB::table('properties')->count(),
 
-            // ads
+            //revenue
+            'totalpayment' => $todayspay,
+            'thisMonthPay' => $thisMonthPay,
+            'lastMonthPay' => $lastMonthPay,
+            'sixMonthPay' => $sixMonthPay,
+
+            // Free and Paid ads
             'pendingPaidAd' => DB::table('property_payments')->where('payment_status','0')->count(),
             'confirmedPaidAd' => DB::table('property_payments')->where('payment_status','1')->count(),
             'pendingFreeAd' => DB::table('properties')->where('status', 0)->whereNotIn('id', function ($query) {
@@ -27,12 +41,12 @@ public function index()
                 $query->select('property_id')->from('property_payments');
             })->count(),
 
-
-            'todayOrders' => Property::latest()->whereDate('created_at', Carbon::today())->count(),
-            'weekOrders' =>  Property::latest()->whereBetween('created_at', [Carbon::now()->startOfWeek(),Carbon::now()->endOfWeek()])->count(),
-            'monthOrders' =>  Property::latest()->where('created_at', '>=', Carbon::now()->startOfMonth())->count(),
-            'yearOrders' =>  Property::latest()->where('created_at', '>=', $sixMonthsAgo)->count(),
-            'thisMonthPayment' => $thisMonthPay
+            //Total Ads
+            'todayAds' => Property::latest()->whereDate('created_at', Carbon::today())->count(),
+            'weekAds' =>  Property::latest()->whereBetween('created_at', [Carbon::now()->startOfWeek(),Carbon::now()->endOfWeek()])->count(),
+            'monthAds' =>  Property::latest()->where('created_at', '>=', Carbon::now()->startOfMonth())->count(),
+            'yearAds' =>  Property::latest()->where('created_at', '>=', $sixMonthsAgo)->count()
+            
         );
 
         return view('backend.dashboard',compact('data'));
