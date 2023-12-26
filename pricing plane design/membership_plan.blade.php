e36fc6287a3a3f7cc7f7578efd3e5b412ee8a2c4
habib372 <habibsoft372@gmail.com>
2023-12-26 20:12:45 +0600

    updated

---
+++ pricing plane design/membership_plan.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="membership_plan.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+        <div class="pricing-section">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="sec-title text-center">
+                        <span class="title">Get plan</span>
+                        <h2>Choose a Plan</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="outer-box">
+                <div class="row">
+                    <!-- Pricing Block -->
+                    <div class="pricing-block col-lg-3 col-md-3 col-sm-12 wow fadeInUp">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <div class="icon-outer"><i class="fas fa-rocket"></i></div>
+                            </div>
+                            <div class="price-box">
+                                <div class="title">Silver</div>
+                                <h4 class="price">300 Taka</h4>
+                            </div>
+                            <ul class="features">
+                                <li class="true">Conference plans</li>
+                                <li class="true">Free Lunch And Coffee</li>
+                                <li class="true">Certificate</li>
+                                <li class="false">Easy Access</li>
+                                <li class="false">Free Contacts</li>
+                            </ul>
+                            <div class="btn-box">
+                                <a href="https://codepen.io/anupkumar92" class="theme-btn">BUY plan</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Pricing Block -->
+                    <div class="pricing-block col-lg-3 col-md-3 col-sm-12 wow fadeInUp" data-wow-delay="400ms">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <div class="icon-outer"><i class="fas fa-gem"></i></div>
+                            </div>
+                            <div class="price-box">
+                                <div class="title">Gold</div>
+                                <h4 class="price">600 Taka</h4>
+                            </div>
+                            <ul class="features">
+                                <li class="true">Conference plans</li>
+                                <li class="true">Free Lunch And Coffee</li>
+                                <li class="true">Certificate</li>
+                                <li class="true">Easy Access</li>
+                                <li class="false">Free Contacts</li>
+                            </ul>
+                            <div class="btn-box">
+                                <a href="https://codepen.io/anupkumar92" class="theme-btn">BUY plan</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Pricing Block -->
+                    <div class="pricing-block col-lg-3 col-md-3 col-sm-12 wow fadeInUp" data-wow-delay="800ms">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <div class="icon-outer"><i class="icofont-cop-badge"></i></div>
+                            </div>
+                            <div class="price-box">
+                                <div class="title">Platinum</div>
+                                <h4 class="price">1000 Taka</h4>
+                            </div>
+                            <ul class="features">
+                                <li class="true">Conference plans</li>
+                                <li class="true">Free Lunch And Coffee</li>
+                                <li class="true">Certificate</li>
+                                <li class="true">Easy Access</li>
+                                <li class="true">Free Contacts</li>
+                            </ul>
+                            <div class="btn-box">
+                                <a href="https://codepen.io/anupkumar92" class="theme-btn">BUY plan</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Pricing Block -->
+                    <div class="pricing-block col-lg-3 col-md-3 col-sm-12 wow fadeInUp" data-wow-delay="800ms">
+                        <div class="inner-box">
+                            <div class="icon-box">
+                                <div class="icon-outer"><i class="fas fa-handshake"></i></div>
+                            </div>
+                            <div class="price-box">
+                                <div class="title">Friend of FCB</div>
+                                <h4 class="price">1500 Taka</h4>
+                            </div>
+                            <ul class="features">
+                                <li class="true">Conference plans</li>
+                                <li class="true">Free Lunch And Coffee</li>
+                                <li class="true">Certificate</li>
+                                <li class="true">Easy Access</li>
+                                <li class="true">Free Contacts</li>
+                            </ul>
+                            <div class="btn-box">
+                                <a href="https://codepen.io/anupkumar92" class="theme-btn">BUY plan</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
+
