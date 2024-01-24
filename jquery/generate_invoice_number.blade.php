06e73cc81e4e47d071039134e7273f5c024fd2b6
habib372 <habibsoft372@gmail.com>
2024-01-24 20:03:58 +0600

    updated

--- jquery/generate_invoice_number.blade.php
+++ jquery/generate_invoice_number.blade.php
@@ -3,15 +3,18 @@
 
 
     // DC-240123002   like 'DC-YYMMDDCCC  | YYMMDD = date ,  CCC = count total (001,002,003)
-    private function generateInvoiceId(){
+     private function generateInvoiceId()
+    {
+        $sl = MembershipPayment::where('created_at', 'like', '%' . date('Y-m-d') . '%')->where('status', 'Complete')->count() + 1;
+        // or
+        $sl = MembershipPayment::whereDate('created_at', '=', now()->toDateString())->where('status', 'Complete')->count() + 1;
 
-        $sl = AppointmentBill::where('created_at', 'like', '%'.date('Y-m-d').'%')->count() + 1;
-        return 'DC-'.date('ymd').str_pad($sl,3,'0', STR_PAD_LEFT);
-
+        return 'OP-' . date('ymd') . str_pad($sl, 3, '0', STR_PAD_LEFT);
     }
 
     //rendom number generate
     $random_user = rand(111111, 999999);
+
 
 
 
