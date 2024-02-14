12f8f0b1cdc08aa752b9d21255fbf5f1ec453404
habib372 <habibsoft372@gmail.com>
2024-02-14 19:09:36 +0600

    updated

---
+++ ERROR HANDLING/server_errors.blade.php
@@ -0,0 +1,7 @@
+{{--
+This page isn’t workingwatertankcleaningbd.com is currently unable to handle this request.
+HTTP ERROR 500 --}}
+check .htaccess and composer.json. if version is 7.1.0 and laravel 5, then open
+1. Multiphp Manager > set version: PHP 7.4 (ea-php74)
+2. Select PHP Version > action > Click (User Multiphp Manager)
+select Current PHP version 7.2 >apply
