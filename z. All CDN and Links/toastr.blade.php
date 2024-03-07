cdcee9ac7b367873fd785b0293de8dd932ea8413
habib372 <habibsoft372@gmail.com>
2024-03-07 14:57:58 +0600

    updated

--- z. All CDN and Links/toastr.blade.php
+++ z. All CDN and Links/toastr.blade.php
@@ -1,46 +1,63 @@
-<!-- Include Bootstrap CSS -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+https://github.com/brian2694/laravel-toastr
 
-<!-- Include Toastr CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-<!-- Include your application CSS -->
-
-<!-- Include Toastr JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-<!-- Include your application JS -->
-
-{{-- composer install --}}
+1.  <!-- composer install -->
 composer require brian2694/laravel-toastr
 npm run dev
 
-{{-- // Example Toastr initialization --}}
-<script>
-    toastr.options = {
-        "closeButton": true,
-        "progressBar": true,
-        // Add other options as needed
-    };
-</script>
+
+2. <!-- Add the service provider to config/app.php  -->
+Brian2694\Toastr\ToastrServiceProvider::class,
+
+
+3. <!-- Optionally include the Facade in config/app.php if you'd like. -->
+'Toastr'  => Brian2694\Toastr\Facades\Toastr::class,
+
+
+4. <!-- You can set custom options for Reminder. Run: -->
+php artisan vendor:publish
+
+
+5. <!-- Controller/route -->
+Route::get('/', function () {
+    Toastr::success('Messages in here', 'Title', ["positionClass" => "toast-top-center"]);
+    return view('welcome');
+});
+
+
+6. <!-- view file (root) -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+		<script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+        <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+
+        {!! Toastr::message() !!}
+
+        <!-- custom css -->
+        <script>
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true,
+                // Add other options as needed
+            };
+        </script>
+
+    </body>
+</html>
 
 
 
-{{--// Example in a controller --}}
-    public function someAction()
-    {
-        toastr()->success('Success message', 'Title');
-        return view('some.view');
-    }
 
-
-
-{{-- Example in a Blade view --}}
-@if(session('success'))
-    <script>
-        toastr.success("{{ session('success') }}", "Success");
-    </script>
-@endif
 
 
 
