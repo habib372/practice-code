a1e6ec0e0a6425795ec5e44cac2db178474b4ac6
habib372 <habibsoft372@gmail.com>
2024-05-19 17:57:36 +0600

    updated

---
+++ PDF generate/generate_pdf.blade.php
@@ -0,0 +1,150 @@
+
+<?php
+
+
+//composer install
+composer require barryvdh/laravel-dompdf
+
+
+//  Route file
+Route::get('/generate-pdf', 'PDFController@download');
+
+// PDFController.php
+use Barryvdh\DomPDF\Facade\Pdf;
+
+public function download()
+    {
+        set_time_limit(120);
+
+        $data = [
+            [
+                'quantity' => 1,
+                'description' => '1 Year Subscription',
+                'price' => '129.00'
+            ]
+        ];
+
+        $pdf = Pdf::loadView('generate_pdf', ['data' => $data]);
+
+        return $pdf->download('prescription.pdf');
+    }
+
+?>
+
+
+<!-- view file design -->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Invoice</title>
+    <style>
+        h4 {
+    margin: 0;
+}
+.w-full {
+    width: 100%;
+}
+.w-half {
+    width: 50%;
+}
+.margin-top {
+    margin-top: 1.25rem;
+}
+.footer {
+    font-size: 0.875rem;
+    padding: 1rem;
+    background-color: rgb(241 245 249);
+}
+table {
+    width: 100%;
+    border-spacing: 0;
+}
+table.products {
+    font-size: 0.875rem;
+}
+table.products tr {
+    background-color: rgb(96 165 250);
+}
+table.products th {
+    color: #ffffff;
+    padding: 0.5rem;
+}
+table tr.items {
+    background-color: rgb(241 245 249);
+}
+table tr.items td {
+    padding: 0.5rem;
+}
+.total {
+    text-align: right;
+    margin-top: 1rem;
+    font-size: 0.875rem;
+}
+    </style>
+</head>
+<body>
+    <table class="w-full">
+        <tr>
+            <td class="w-half">
+                <!-- convert base64 -->
+                <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('mediroaming.png'))) }}" alt="mediroaming" width="200">
+            </td>
+            <td class="w-half">
+                <h2>Invoice ID: 834847473</h2>
+            </td>
+        </tr>
+    </table>
+
+    <div class="margin-top">
+        <table class="w-full">
+            <tr>
+                <td class="w-half">
+                    <div><h4>To:</h4></div>
+                    <div>John Doe</div>
+                    <div>123 Acme Str.</div>
+                </td>
+                <td class="w-half">
+                    <div><h4>From:</h4></div>
+                    <div>Laravel Daily</div>
+                    <div>London</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="margin-top">
+        <table class="products">
+            <tr>
+                <th>Qty</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+            <tr class="items">
+                @foreach($data as $item)
+                    <td>
+                        {{ $item['quantity'] }}
+                    </td>
+                    <td>
+                        {{ $item['description'] }}
+                    </td>
+                    <td>
+                        {{ $item['price'] }}
+                    </td>
+                @endforeach
+            </tr>
+        </table>
+    </div>
+
+    <div class="total">
+        Total: $129.00 USD
+    </div>
+
+    <div class="footer margin-top">
+        <div>Thank you</div>
+        <div>&copy; Laravel Daily</div>
+    </div>
+</body>
+</html>
