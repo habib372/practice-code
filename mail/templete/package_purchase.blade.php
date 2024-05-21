633a37b8e74c55379fbe9197d05ab56b854f8706
habib372 <habibsoft372@gmail.com>
2024-05-21 16:16:11 +0600

    updated

--- mail/templete/package_purchase.blade.php
+++ mail/templete/package_purchase.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Example</title>
+    <title>[Company Name]</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -63,10 +63,10 @@
             color:#fff;
             background:#176db8;
         }
-        h4{
+         h4{
             color:#176db8;
         }
-        h3{
+          h3{
             color:#176db8;
             margin:0;
             font-size:16px;
@@ -82,7 +82,18 @@
         span{
             font-size:15px;
         }
-        .no_reply{
+        .signup{
+            text-align: center;
+            margin:25px;
+        }
+        .login-button{
+            padding: 10px 20px;
+            background-color: #05bcd6;
+            color: #fff!important;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+       .no_reply{
             text-align:center;
             font-weight: bold;
         }
@@ -98,34 +109,41 @@
 <body>
     <div class="container">
         <div class="header">
-            <img height="50" src="https://example.com/uploads/header/5.png" alt=" Logo">
+            <img height="50" src="https://example.com/uploads/header/medilogo.png" alt=" Logo">
         </div>
 
 
         <h2>Dear {{$name}},</h2><br/>
 
-        <p>Your appointment with [Company Name]  on {{$appointment_date}} is confirmed. Here are the details :</p>
+        <p>You have successfully purchased a tour cover package. Here are the details :</p><br/>
 
-        <br/>
-
-        <div class="appointment_mail_info">
-            <span>Appointment Type : <b> {{$appointment_type}} consultation</b></span><br/>
-            <span>Appointment Date : <b> {{$appointment_date}} </b></span><br/>
-            <span>Appointment Start Time : <b> {{$start_time}} </b></span><br/>
-            <span>Appointment End Time :  <b>{{$end_time}}</b></span><br/>
-            <span>Location :  address</span>
+        <div class="purcahse_mail_info">
+            <span>Your Name : <b> {{$name}} </b></span><br/>
+            <span>Invoice ID : <b> {{$invoice_id}}</b></span><br/>
+            <span>Package Type : <b> {{$package_type}} Package</b></span><br/>
+            <span>Package Coverage :  <b>{{$package_coverage}}</b></span><br/>
+            <span>Package Start Time :  <b>{{$package_start_date}}</b></span><br/>
+            <span>Package End Time :  <b>{{$package_end_date}}</b></span>
         </div>
 
         <br/><br/>
 
-        <p>If you need to reschedule, cancel or setup online telehealth appointment, feel free to contact our support team.
+        <p>Enjoy your journey with [Company Name]. You can now book an appointment for Doctor Consultation or online telehealth. Please read the Product Disclosure Statement (PDS) carefully to ensure it provides the cover you need.
         </p>
+
+        <div class="signup">
+            <a class="login-button" href="https://example.com/uploads/pds/pds-2024.pdf" role="button">Download PDS</a>
+        </div>
+
+        <p>If you have any questions or feedback, please contact our support team at</p>
         <span>Email: info@example.com</span><br/>
         <span>Phone: +880 17xx xxx xxx</span>
 
         <br/><br/>
 
-        <p>Thank you for choosing [Company Name]. We look forward to providing you with top-notch health care services!</p><br/>
+        <p>Thank you once again for choosing [Company Name]. We look forward to providing you with top-notch health care services!</p>
+
+        <br/>
 
         <p> Best Regards, </p>
         <h3>[Company Name]</h3>
@@ -133,7 +151,7 @@
         <br/><br/>
 
         <div class="no_reply">
-           <i>(This is an auto-generated email from [Company Name]. Do not reply to this email as the mailbox is not monitored)</i>
+           <i>(This is an auto-generated email from [Company Name]. Do not reply to this email as the mailbox is not monitored.)</i>
         </div>
 
          <br/> <br/>
