f2582254b271985924a3113d19a1c0d2c32e06ca
habib372 <habibsoft372@gmail.com>
2024-08-14 10:31:49 +0600

    updated

---
+++ app2.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <div class="card-body pb-0">
+        <div class="card-img">
+
+            <div uk-lightbox="">
+                <a href="https://educareer.demo.sharirbazar.com/uploads/gallery/13.jpg" target="_blank">
+                    <img src="https://educareer.demo.sharirbazar.com/uploads/gallery/13.jpg" class="img-fluid" alt=""></a>
+            </div>
+        </div>
+        <div class="card-title d-block">
+            <p class="text-center mt-3" style="color:#161616">Gallery</p>
+        </div>
+    </div>
+</div>
