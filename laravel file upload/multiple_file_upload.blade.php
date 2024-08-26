9f3ea89cbeb0ac81726c089829fb9d0cac3819df
habib372 <habibsoft372@gmail.com>
2024-08-26 14:37:04 +0600

    updated

--- laravel file upload/multiple_file_upload.blade.php
+++ laravel file upload/multiple_file_upload.blade.php
@@ -1,6 +1,12 @@
+<div class="col-lg-6 col-md-6 col-sm-12">
+	<label>Add More Product Photo </label>
+	<div class="custom-file">
+		<input type="file" class="form-control" name="images[]" multiple>
+	</div>
+</div>
+
+
 <?php
-
-
 
 
 // Edit image
