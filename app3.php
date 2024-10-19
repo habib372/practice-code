e30141cae1f6f112e953f1ca3484378f5ffa3a99
habib372 <habibsoft372@gmail.com>
2024-10-20 04:41:03 +0600

    updated

---
+++ app3.php
@@ -0,0 +1,33 @@
+
+.head2_mid_logo {
+    display: flex;
+}
+.logo_subtitle {
+    margin-left: 20px;
+}
+.logo_subtitle h2 {
+    margin: 10px 0 0;
+    font-weight: 700;
+}
+.logo_subtitle h2 {
+    margin: 10px 0 0;
+    font-weight: 700;
+}
+.logo_subtitle p {
+    margin: 0 !important;
+    font-family: monospace;
+    letter-spacing: 0.22rem;
+}
+
+
+<div class="head2_mid_logo">
+    <div class="logo_">
+        <a href="https://shahajadi.demo.sharirbazar.com" class="d-none d-md-block ada">
+            <img src="https://shahajadi.demo.sharirbazar.com/uploads/header/5.png" alt="">
+        </a>
+    </div>
+    <div class="logo_subtitle">
+        <h2>SHAHAJADI AGENCY</h2>
+        <p>E-visa Immigration Services</p>
+    </div>
+</div>
