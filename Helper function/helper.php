fb9a0823bb62a2ac98f8b3c6a170a1fcf4b2481d
habib372 <habibsoft372@gmail.com>
2024-11-27 17:04:20 +0600

    updated

--- Helper function/helper.php
+++ Helper function/helper.php
@@ -1,5 +1,10 @@
 <?php
 
+
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
 // function prx($arr)
 // {
 //     echo "<pre>";
@@ -24,4 +29,77 @@ function engToBn($number){
 
     $bn_number = strtr($number,$eng_to_bn);
     return $bn_number;
+}
+
+
+// Date to age convert - 10Y 5M
+function calculateAgeToday($date)
+{
+    //Take patient's dob and return age today
+    if (empty($date)) {
+        return '';
+    }
+    $datetime1 = new DateTime($date);
+    $datetime2 = new DateTime(date('Y-m-d'));
+    $interval = $datetime1->diff($datetime2);
+
+    return $interval->format('%yY %mM');
+}
+
+function convertDateTime($datetime)
+{
+    $dateTimeObj = new DateTime($datetime);
+
+    // Format the DateTime object to the desired format- 2024-02-22 02:30 PM
+    $formattedDateTime = $dateTimeObj->format('Y-m-d h:i A');
+
+    return $formattedDateTime;
+}
+
+// Format the DateTime object to the desired format- 25th April, 2024
+if (!function_exists('formatDate')) {
+    function formatDate($date)
+    {
+        return Carbon::parse($date)->isoFormat('Do MMMM, YYYY');
+    }
+}
+
+// Format the DateTime object to the desired format- 02:30 PM
+if (!function_exists('formatTime')) {
+    function formatTime($time)
+    {
+        return Carbon::parse($time)->format('h:i A');
+    }
+}
+
+
+//get USER Data and IP address base on Country
+function getGeolocationData($user_ip)
+{
+    if ($user_ip) {
+        $ip = $user_ip;
+    } else {
+        $ip = $_SERVER["REMOTE_ADDR"];
+    }
+    // Make a request to the IP geolocation service
+    $client = new Client();
+    $response = $client->get('https://api.ipgeolocation.io/ipgeo?apiKey=024d33e025f2425fbd81f274acb3737b&ip=' . $ip);
+
+    // Parse the response JSON
+    $data = json_decode($response->getBody(), true);
+
+    // Extract the required information
+    $geolocation = [
+        'ip' => $data['ip'],
+        'country_name' => $data['country_name'],
+        'country_code2' => $data['country_code2'],
+        'country_code3' => $data['country_code3'],
+        'currency_code' => $data['currency']['code'],
+        'currency_name' => $data['currency']['name'],
+        'currency_symbol' => $data['currency']['symbol'],
+        'timezone' => $data['time_zone']['name'],
+        'current_time' => $data['time_zone']['current_time']
+    ];
+
+    return $geolocation;
 }
