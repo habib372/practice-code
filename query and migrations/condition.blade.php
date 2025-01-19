f0f719c653c88240c8d0f2844c3547be4eabea15
habib372 <habibsoft372@gmail.com>
2025-01-19 12:41:52 +0600

    updated

--- query and migrations/condition.blade.php
+++ query and migrations/condition.blade.php
@@ -111,6 +111,10 @@ if (preg_match('#^(en|bn)/promote-ad/.*$#', $uri)) {
 <!-- skip first data -->
 $memberships = ['' => '-- Select a membership --'] + Membership::where('status', 'active')->pluck('name_en', 'id')->slice(1)->toArray();
 
+$allData = Medicine::skip(1)->take(PHP_INT_MAX)->get(); <!--Skip the first record and get the collection-->
+$allData = Medicine::all()->slice(1);<!-- Skip the first record in the collection-->
+$allData = Medicine::all()->slice(1,10);<!-- take 1 to 10 record in the collection and skip all others-->
+
 
 <!-- multiple disease by id -->
  $patientVisit = PatientVisit::with(['patient.disease', 'patient.chemotherapies' => function($query){
