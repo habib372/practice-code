c9df83560af3e7f1366319fcd0a7e966372b7277
habib372 <habibsoft372@gmail.com>
2025-02-19 14:57:51 +0600

    updated

--- mail/email/email_schedule.blade.php
+++ mail/email/email_schedule.blade.php
@@ -1,3 +1,12 @@
 <!-- Email Schedule command | app/Console/Commands/SendBirthdayEmails.php-->
 php artisan make:command SendBirthdayEmails
 
+
+
+
+Test::
+login cpanel using terminal or cmd->
+ssh user@92.208.158.155 -p2222      //cpnael_userName@cpanel_ip  -p2222(default port),
+password
+cd ~/folder_name                    //folder_name: 'public_html' or 'sub_domain_folder_name'
+php artisan emails:send-birthday    //command name: 'emails:send-birthday'
