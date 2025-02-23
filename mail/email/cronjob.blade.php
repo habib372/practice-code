8a53df2a12f341927be4267eed1f779377f117b7
habib372 <habibsoft372@gmail.com>
2025-02-24 02:07:52 +0600

    updated

--- mail/email/cronjob.blade.php
+++ mail/email/cronjob.blade.php
@@ -3,4 +3,4 @@
 
 * * * *	*  /usr/local/bin/ea-php82 /home2/username/folder_path/artisan schedule:run >> /dev/null 2>&1  (cpanel)
 
-* * * * * /home/userName/domains/domain_name/public_html/artisan schedule:run >> /dev/null 2>&1  (metrovps)
+* * * * * php /home/userName/domains/domain_name/public_html/artisan schedule:run >> /dev/null 2>&1  (metrovps)
